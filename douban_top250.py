1f32ab92a28f5c137de98a8f8f3f04d7a2b07477
Jazz cat <1348842185@qq.com>
2023-07-27 14:49:14 +0800

    基本功能完成

--- douban_top250.py
+++ douban_top250.py
@@ -21,30 +21,35 @@ def get_music_url(url):
     music_hrefs = selector.xpath('//a[@class = "nbg"]/@href')
     for music_href in music_hrefs:
         get_music_info(music_href)
+    #---------------------------
 
 #这个用来获取每首歌的详细信息
 #para:从get_music_url()获取的url
 def get_music_info(url):
     html = requests.get(url,headers = headers)
     selector = etree.HTML(html.text)
-    name = selector.xpath('//*[@id="wrapper"]/h1/span/text()')
-    player = selector.xpath('//*[@id="info"]/span[1]/span/text()')
-    publish_time = selector.xpath('//*[@id="info"]/text()[4]')
-    genere = selector.xpath('//*[@id="info"]/text()[1]')
+    name = selector.xpath('//*[@id="wrapper"]/h1/span/text()')[0]
+    player = re.findall('表演者:.*?>(.*?)<',html.text,re.S)[0]
+    #publish_time = selector.xpath('//*[@id="info"]/text()')
+    publish_time = re.findall('发行时间:.*?;(.*?)\s',html.text,re.S)[0]
+    genere = re.findall('流派:.*?;(.*?)\s',html.text,re.S)
     if len(genere) == 0:
         genere = '未知'
     else:
         genere = genere[0].strip()
-    publishers = re.findall('出版者:.*?>(.*?)</a>',html.text,re.S)
+    publishers = re.findall('出版者:.*?;(.*?)\s',html.text,re.S)
     if len(publishers) == 0:
         publisher = '未知'
     else:
         publisher = publishers[0].strip()
     score = selector.xpath('//*[@id="interest_sectl"]/div/div[2]/strong/text()')[0]
-    print(name,player,genere,publish_time,publisher,score)
+    #----------------------------------
+    content = etree.tostring(selector)
+    print("%s\n%s\n%s\n%s\n%s\n%s" %(name,player,genere,publish_time,publisher,score))
     info = {
         'name':name,
         'player':player,
+        'style':genere,
         'time':publish_time,
         'publisher':publisher,
         'score':score
@@ -52,7 +57,7 @@ def get_music_info(url):
     musictop.insert_one(info)
 
 if __name__ == '__main__':
-    urls = ['https://music.douban.com/top250?start={}'.format(str(i)) for i in range(0,25,25)]
-    for url in urls:
+    urls = ['https://music.douban.com/top250?start={}'.format(str(i)) for i in range(0,250,25)]
+    for url in urls: #这是榜单页面的url
         get_music_url(url)
         time.sleep(2)
